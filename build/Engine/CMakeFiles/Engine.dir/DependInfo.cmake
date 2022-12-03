790490690e04f2f06c01938868971f19f8e65d16
Myrzabek Alizhanov <mlgexgood@gmail.com>
2022-12-04 03:43:11 +0600

    Project Init

---
+++ build/Engine/CMakeFiles/Engine.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/Projects/C++/Fun/sfml/SFML_PingPong/Engine/src/EngineCore/Utils/test.cpp" "F:/Projects/C++/Fun/sfml/SFML_PingPong/build/Engine/CMakeFiles/Engine.dir/src/EngineCore/Utils/test.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Engine/includes"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Game/CMakeFiles/PingPongEditor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../bin/PingPongEditor.exe"
+  "../bin/PingPongEditor.exe.manifest"
+  "../bin/PingPongEditor.pdb"
+  "CMakeFiles/PingPongEditor.dir/src/main.cpp.obj"
+  "libPingPongEditor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PingPongEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
